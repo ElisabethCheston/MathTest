4fe5490039aee9376e281aef7e91ed2ef01f2dd0
ElisabethCheston <elisabeth.cheston@gmail.com>
2020-12-04 16:21:17 +0000

    new layout

--- js/game.js
+++ js/game.js
@@ -1,50 +1,47 @@
-// Making the rows for the bricks //
-const game = () => {
-    const rows = 4;
-    const columns = 4;
 
+// Create a varible for all the innerbricks. //
+let bricks = document.querySelectorAll('innerbrick')
 
-    // We have to create 2 variables; 1 for the content in the 
-    // Container ID for the game.
-const containerId = 'game';
+let firstClick = false
+let counter = 0
+let brickPair = []
 
+bricks.forEach((brick) => {
+    brick.state = 'unclicked'
+})
 
-// Making a loop to write the bricks out. //
-for (let i = 0; i < rows * columns; i++)
-};
+shuffle()
 
-// Write them all out like this and try to merge them in shuffle.. //
-var multi1 = ["21", "3 x 7"]
-var multi2 = ["9", "3 x 3"]
-var multi3 = ["24", "8 x 3"]
-var multi4 = ["18", "2 x 9"]
-var multi5 = ["12", "3 x 4"]
-var multi6 = ["70", "10 x 7"]
-var multi7 = ["40", "5 x 8"]
-var multi8 = ["36", "6 x 6"]
+function shuffle() {
+    let images = document.querySelectorAll('img')
+    let image = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8]
 
+    //functions
+function giveNumbers() {
+    for (i = 0; i < buttons.length; i++) {
+        buttons [i].dataset.number = numbers[i];
+    }
+}
+// https://medium.com/@nitinpatel_20236/how-to-shuffle-correctly-shuffle-an-array-in-javascript-15ea3f84bfb //
 
-var brick1 = ["house", "casa"]
-var brick2 = ["window", "ventana"]
-...
-// Or something like.. //
-var image = [["house", "casa"], ["window", "ventana"], ["water", "agua"],...]
-
-// ..can shuffle the bricks //
-
-
+function shuffle(array) {
+    var j, x, i;
+for(i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const x = array[i]
+    array[i] = array[j]
+    array[j] = x
+  }
+  return array;
+}
+}
 
 /*
-//variables
-var match1 = ["21", "3 x 7"]
-var match2 = ["9", "3 x 3"]
-
-var image = [[1, 1], [2, 2], [3, 3], [4, 4], [5, 5], [6, 6], [7, 7], [8, 8]]
+var image = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8]
 var lastKnownButtonId = undefined;
 var lastKnownButtonNumber = undefined;
 var wait = false;
 var matches = 0;
-
 
 //elements
 var buttons = document.querySelectorAll("button");
