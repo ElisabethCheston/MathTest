393964df79c411576be0f2e29049ac9ba9cc6311
ElisabethCheston <elisabeth.cheston@gmail.com>
2020-12-04 21:13:10 +0000

    test

--- js/game.js
+++ js/game.js
@@ -1,21 +1,106 @@
-
+/*
 // Create a varible for all the innerbricks. //
-let bricks = document.querySelectorAll('innerbrick')
+let cards = document.querySelectorAll('.innerbrick');
 
 let firstClick = false
 let counter = 0
-let brickPair = []
+let brickPair = [] 
 
-bricks.forEach((brick) => {
-    brick.state = 'unclicked'
+function turn() {
+
+}
+cards.forEach((card) => {
+    card.addEventListener('click', turn)
 })
 
 shuffle()
 
+for(let i = 0; i < cards.length; i++) {
+    cards[i].addEventListener('click',() => {
+        if(!firstClick){time()}
+        firstClick = true
+
+        if(cards[i].state == 'unclicked') {
+            cards[i].style.transform = 'rotateY(180deg)'
+            cards[i].state = 'clicked'
+            counter++
+            cardPair.push(cards[i])
+            check()
+        }
+        else if (cards[i].state == 'clicked') {
+            cards[i].style.transform = 'rotateY(0deg)'
+            cards[i].state = 'unclicked'
+            counter --
+            cardPair = []
+        }
+    })
+}
+
+function check() {
+    if(counter==2) {
+        if(cardPair[0].querySelector('img').src == cardPair[1].querySelector('img').src) {
+            matched()
+        }
+        else {
+            unmatched(cardPair[0], cardPair[1])
+        }
+    }
+}
+
+function matched() {
+    cardPair[0].state = 'blocked'
+    cardPair[1].state = 'blocked'
+    counter = 0
+    cardPair = []
+    let score = document.querySelector('#score').innerHTML
+    score++
+    document.querySelector('#score').innerHTML = score
+}
+function unmatched(x, y) {
+    setTimeout(() => {
+        x.style.transform = 'rotateY(0deg)'
+        y.style.transform = 'rotateY(0deg)'
+    }, 750)
+    cardPair[0].state = 'unclicked'
+    cardPair[1].state = 'unclicked'
+    counter = 0
+    cardPair = []
+}
+
+// Making a function for timing the game. //
+function time() {
+    let secs = 0
+    let mins = 0
+    let SS
+    let MM
+    setInterval(() => {
+        secs++
+        if(secs == 60) { secs = 0; mins++}
+
+        secs <10?SS = `0${secs}`: SS = `${secs}`
+        mins <10?SS = `0${mins}`: SS = `${mins}`
+
+        document.querySelector('#time').innerHTML = `${MM}:${SS}`
+    }, 1000);
+}
+
+// Shuffle function //
 function shuffle() {
     let images = document.querySelectorAll('img')
-    let image = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8]
+    let srcs = ["test.png", "test.png", "test.png", "test.png", "test.png", "test.png", "test.png", "test.png", "test.png", "test.png", "test.png", "test.png", "test.png", "test.png", "test.png", "test.png",]
 
+    for(i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * i);
+    const x = srcs[i]
+    srcs[i] = srcs[j]
+    srcs[j] = x
+    }
+
+    for (let i = 0; i < images.length; i++) {
+        images[i].src = srcs[i];
+    }
+}
+     /*
     //functions
 function giveNumbers() {
     for (i = 0; i < buttons.length; i++) {
